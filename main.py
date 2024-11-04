5f65d00b146e3d26a9082b18fd7aa3f03c42452c
Kishok-S-123 <kishoksiva123@gmail.com>
2024-11-04 15:03:11 +0000

    team colour clustering

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
-
+import torch
+import cv2
 
 model = YOLO("trained_models/best.pt")
 tracker = sv.ByteTrack()
@@ -10,21 +11,74 @@ label_annotator = sv.LabelAnnotator()
 
 
 
-def callback(frame: np.ndarray, _: int):
-    results = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(results)
-    detections = tracker.update_with_detections(detections)
+# def callback(frame: np.ndarray, _: int):
+#     results = model(frame)[0]
+#     detections = sv.Detections.from_ultralytics(results)
+#     detections = tracker.update_with_detections(detections)
 
-    labels = [
-        f"{tracker_id} {results.names[class_id]}"
-        for class_id, tracker_id 
-        in zip(detections.class_id, detections.tracker_id)
-        ]
+#     labels = [
+#         f"{tracker_id} {results.names[class_id]}"
+#         for class_id, tracker_id 
+#         in zip(detections.class_id, detections.tracker_id)
+#         ]
     
-    ellipse_frame = ellipse_annotator.annotate(frame.copy(), detections= detections)
+#     ellipse_frame = ellipse_annotator.annotate(frame.copy(), detections= detections)
 
-    return label_annotator.annotate(ellipse_frame.copy(), detections = detections, labels = labels)
+#     return label_annotator.annotate(ellipse_frame.copy(), detections = detections, labels = labels)
+
+## collecting detection boxes:
+"""
+1. Collect bounding box information on all detections of player
+2. perform kmeans clustering of subsection of image corresponding with box coordinates
+ 
+"""
+
+## 1.
 
 
-sv.process_video(source_path="Clips/0bfacc_2.mp4", target_path = "tracked/result.mp4", callback = callback)
+# cap = cv2.VideoCapture('Clips/0bfacc_2.mp4')
+# while cap.isOpened():
+#     ret, frame = cap.read()
+
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+    
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     cv2.imwrite("assets/muhframe.jpg", frame)
+#     cap.release()
+#     break
+
+
+
+
+########
+
+playerBoxes = [torch.Tensor.cpu(box.xyxy) for box in model("assets/muhframe.jpg")[0].boxes if box.cls == torch.tensor([2.], device="cuda:0")]
+print(f"player detections: {playerBoxes}")
+
+sample_frame = cv2.imread("assets/muhframe.jpg")
+sample_frame = cv2.cvtColor(sample_frame, cv2.COLOR_BGR2RGB)
+
+print(sample_frame.shape)
+print(np.shape(playerBoxes[0]))
+print(playerBoxes[0])
+# print(f"playerBoxes[0]: {playerBoxes[0][0][0]}")
+print(f"x1, y1: {playerBoxes[0][0][0]}, {playerBoxes[0][0][1]}")
+print(f"x2, y2: {playerBoxes[0][0][2]}, {playerBoxes[0][0][3]}")
+
+sample_player = sample_frame[int(playerBoxes[0][0][1]):int(playerBoxes[0][0][3]) , int(playerBoxes[0][0][0]):int(playerBoxes[0][0][2]) ,:]
+
+cv2.imwrite("assets/sample_player_img.jpg", sample_player)
+
+
+
+
+
+## 2.
+
+
+
+# sv.process_video(source_path="Clips/0bfacc_2.mp4", target_path = "tracked/result.mp4", callback = callback)
 
--- utils/team_assign.py
+++ utils/team_assign.py
@@ -10,33 +10,60 @@ player_img = cv2.cvtColor(player_img, cv2.COLOR_BGR2RGB)
 
 class team_assign:
 
-    def __init__(self, player_img: np.ndarray):
+    def __init__(self, frame: np.ndarray, player_boxes):
+
     
-        self.player_img_height = np.shape(player_img)[0]
-        self.player_img_width = np.shape(player_img)[1]
+        pass
 
-        shirt_img = player_img[:self.player_img_height//2,:,:]
+    
+    def get_sub_image(frame, box):
+        return frame[int(box[0][1]):int(box[0][3]) , int(box[0][0]):int(box[0][2]) ,:]
 
+    def get_player_col(self, player_img):
+
+        player_img_height = np.shape(player_img)[0]
+        player_img_width = np.shape(player_img)[1]
+
+        shirt_img = player_img[:player_img_height//2,:,:]
+        
         # plt.imshow(shirt_img)
         
-        self.kmeans = sklearn.cluster.KMeans(n_clusters=2)
+        kmeans = sklearn.cluster.KMeans(n_clusters=2)
 
-        self.flat_img = shirt_img.reshape((-1,3))
+        flat_img = shirt_img.reshape((-1,3))
+        
+        kmeans.fit(flat_img)
 
-    def kmeans(self):
-        self.kmeans.fit(self.flat_img)
-
-        self.seg_img = self.kmeans.labels_.reshape((self.player_img_height//2, self.player_img_width))
-
+        seg_img = self.kmeans.labels_.reshape((player_img_height//2, player_img_width))
+        
         # plt.imshow(seg_img)
 
-    def shirt_col(self):
-        centroids = self.kmeans.cluster_centers_
+        centroids = kmeans.cluster_centers_
         # based off assumption that the player will not take up the entire bounding box and thus the majority of the corners will be background
-        corner_vals = [self.seg_img[0,0], self.seg_img[0,1], self.seg_img[1,0], self.seg_img[1,1]]
+        corner_vals = [seg_img[0,0], seg_img[0,1], seg_img[1,0], seg_img[1,1]]
         common = max(set(corner_vals), key = corner_vals.count)
-        self.background_col = centroids[common]
-        self.player_col = centroids[common-1]
+        background_col = centroids[common]
+        player_col = centroids[common-1]
+
+        return player_col 
 
         # print(player_col)
 
+
+    def team_cols(self, player_boxes, frame):
+        # taking shirt colour from each of the players in the frame and adding to list
+        shirt_cols = []
+        for player in player_boxes:
+            player_img = self.get_sub_image(frame, player)
+            shirt_col = self.get_player_col(player_img)
+            shirt_col.append(shirt_col)
+
+
+        # perform kmeans on shirt colours 
+
+        colour_kmeans = sklearn.cluster.KMeans(n_clusters = 2)
+        colour_kmeans.fit(shirt_cols)
+
+        self.team_colours[]
+
+        
